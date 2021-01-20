d2d616ca82ba2f48d55305ac4466660a5c19b0c7
L <eleswastaken@email.com>
2021-01-21 00:38:20 +0600

    labels

--- tkmain.py
+++ tkmain.py
@@ -14,36 +14,41 @@ def check_win():
 
 def press(arg):
     global turn
+    print(arg._name)
     if turn == 'first':
         arg.config(image=ximage, width=100, height=100)
         arg.grid()
         arg['state'] = 'disabled'
         turn = 'second'
+        label.config(text="Second's turn.")
     elif turn == 'second':
         arg.config(image=oimage, width=100, height=100)
         arg.grid()
         arg['state'] = 'disabled'
         turn = 'first'
+        label.config(text="First again.")
     check_win()
 
+label = Label(text='First to go!', font=('Arial', 15))
+label.grid(column=0, row=3, columnspan=3)
 right_image = PhotoImage(file='images/sq.png', width=100, height=100)
-button1 = Button(image=right_image, highlightthickness=0, command=lambda: press(button1))
+button1 = Button(name='1',image=right_image, highlightthickness=0, command=lambda: press(button1))
 button1.grid(column=0, row=0)
-button2 = Button(image=right_image, highlightthickness=0, command=lambda: press(button2))
+button2 = Button(name='2',image=right_image, highlightthickness=0, command=lambda: press(button2))
 button2.grid(column=0, row=1)
-button3 = Button(image=right_image, highlightthickness=0, command=lambda: press(button3))
+button3 = Button(name='3',image=right_image, highlightthickness=0, command=lambda: press(button3))
 button3.grid(column=0, row=2)
-button4 = Button(image=right_image, highlightthickness=0, command=lambda: press(button4))
+button4 = Button(name='4',image=right_image, highlightthickness=0, command=lambda: press(button4))
 button4.grid(column=1, row=0)
-button5 = Button(image=right_image, highlightthickness=0, command=lambda: press(button5))
+button5 = Button(name='5',image=right_image, highlightthickness=0, command=lambda: press(button5))
 button5.grid(column=1, row=1)
-button6 = Button(image=right_image, highlightthickness=0, command=lambda: press(button6))
+button6 = Button(name='6',image=right_image, highlightthickness=0, command=lambda: press(button6))
 button6.grid(column=1, row=2)
-button7 = Button(image=right_image, highlightthickness=0, command=lambda: press(button7))
+button7 = Button(name='7',image=right_image, highlightthickness=0, command=lambda: press(button7))
 button7.grid(column=2, row=0)
-button8 = Button(image=right_image, highlightthickness=0, command=lambda: press(button8))
+button8 = Button(name='8',image=right_image, highlightthickness=0, command=lambda: press(button8))
 button8.grid(column=2, row=1)
-button9 = Button(image=right_image, highlightthickness=0, command=lambda: press(button9))
+button9 = Button(name='9',image=right_image, highlightthickness=0, command=lambda: press(button9))
 button9.grid(column=2, row=2)
 
 
